f5047652f514d2912bd087575dc6fd0da6419b15
JoaoPeluzio <jvpeluzio@gmail.com>
2024-02-03 15:47:00 -0300

    atualizacao

--- javascript/spaDesafio/src/index.js
+++ javascript/spaDesafio/src/index.js
@@ -7,27 +7,6 @@ const routes = {
   404: "/pages/404.html",
 }
 
-//to add click event and prevent to refresh and direct the page
-function route(event){
-  event = event || window.event
-  event.preventDefault()
-
-  window.history.pushState({}, "", event.target.href)
-
-  handle()
-}
-
-function handle() {
-  const { pathname } = window.location
-  const route = routes[pathname] || routes[404]
-
-  fetch(route)
-  .then(data => data.text())
-  .then(html => {
-    document.querySelector('#app').innerHTML = html
-  })
-
-}
 
 handle()
 
